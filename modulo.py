0553dba43bd80811c164fab45498c6fd17706270
RUIZ José Antonio <RuizJ@sancristobal.com.ar>
2023-03-13 17:14:23 -0300

    agregue mas funciones y las puse en elk scrip

--- modulo.py
+++ modulo.py
@@ -1,2 +1,5 @@
 def suma(a,b):
     retunr a + b
+
+def resta(a,b):
+    retunr a - b
--- script.py
+++ script.py
@@ -1,1 +1,7 @@
+from modulo import suma, resta
+
 print("Hola mundo")
+
+print(f"la suma de 1 + 2 = {suma(1,2)}")
+
+print(f"la resta de 1 + 2 = {resta(1,2)}")
